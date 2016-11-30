a219f5b833e0e5b5c932b88837403f4d4d93299c
Barry Willis <Barry.Willis@sita.aero>
2016-11-30 11:48:56 +0000

    Maintenance complete - 30/11/2016

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var onSwitch = false;
+var onSwitch = true;
 
 const port = process.env.PORT || 3000;
 var app = express();
